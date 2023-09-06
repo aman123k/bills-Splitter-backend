6b08f9073a40234cba95e4fea08fc34d3215a9ab
aman <sude8920esh@gmail.com>
2023-09-06 14:52:43 +0530

    seven

--- controller/userController.js
+++ controller/userController.js
@@ -39,7 +39,7 @@ class userController {
     if (comparePass) {
       const accessToken = createToken(userDetais);
       res.cookie("accessToken", accessToken, {
-        httpOnly: true,
+        httpsOnly: true,
         secure: true,
         path: "/",
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
