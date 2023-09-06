97fdeb45bb1abb32a32453a2e4b9dab4add10b22
aman <sude8920esh@gmail.com>
2023-09-06 13:52:27 +0530

    five

--- controller/userController.js
+++ controller/userController.js
@@ -45,7 +45,7 @@ class userController {
         expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
         sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24 * 30,
-        domain: "vercel.app",
+        domain: ".vercel.app",
       });
 
       res.status(200).json({
